12ba4098f7859d3872cd5117186629a21634d0cb
Mansurov Bexruz <mansurovbexruz21@gmail.com>
2025-03-24 14:00:12 +0500

    HomePage

--- src/home-page.ts
+++ src/home-page.ts
@@ -7,23 +7,43 @@ const page2 = document.getElementById('PAGE-2') as HTMLDivElement;
 const page3 = document.getElementById('PAGE-3') as HTMLDivElement;
 
 
+const seeAllFeatured = document.getElementById("seeAll-featured") as HTMLAnchorElement;
+const seeAllRecommended = document.getElementById("seeAll-recommended") as HTMLAnchorElement;
+
+
+seeAllFeatured?.addEventListener("click", (event) => {
+    event.preventDefault(); 
+    page2.style.display = 'block';
+    page1.style.display = 'none';
+    page3.style.display = 'none';
+});
+
+seeAllRecommended?.addEventListener("click", (event) => {
+    event.preventDefault(); 
+    page3.style.display = 'block';
+    page1.style.display = 'none';
+    page2.style.display = 'none'; 
+});
+
 homePage?.addEventListener('click', (event) => {
-    event.preventDefault(); // Standart hodisani bloklash
+    event.preventDefault(); 
     page1.style.display = 'block';
     page2.style.display = 'none';
     page3.style.display = 'none';
 });
 
 featured?.addEventListener('click', (event) => {
-    event.preventDefault(); // Standart hodisani bloklash
+    event.preventDefault(); 
     page2.style.display = 'block';
     page1.style.display = 'none';
     page3.style.display = 'none';
 });
 
 recommended?.addEventListener('click', (event) => {
-    event.preventDefault(); // Standart hodisani bloklash
+    event.preventDefault(); 
     page3.style.display = 'block';
     page1.style.display = 'none';
     page2.style.display = 'none';
 });
+
+
